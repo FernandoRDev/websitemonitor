6a93f52a3c5a4356a1ecfe50eeb1cd8abe2b4e90
fernandordev <fernandorodriguesdev@outlook.com>
2020-06-20 14:41:42 -0300

    a lot of changes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello noob");
+const getregister = document.getElementById("register");
+const getlogin = document.getElementById("login");
+
+function login() {
+    if (getlogin.style.display === "block") {
+        getlogin.style.display = "none";
+    } else {
+        if (getregister.style.display === "block") {
+            getregister.style.display = "none";
+        }else{
+            getlogin.style.display = "block";
+            getregister.style.display = "none";            
+        }
+    }
+}
+
+function register() {
+    if (getregister.style.display === "block") {
+        getregister.style.display = "none";
+    } else {
+        getregister.style.display = "block";
+        getlogin.style.display = "none";
+    }
+}
