1da628195f91f5a8ce85f5936bb2b4e796311f14
fernandordev <fernandorodriguesdev@outlook.com>
2020-06-20 16:05:11 -0300

    lightmode

---
+++ theme.js
@@ -0,0 +1,30 @@
+// light Mode
+let lightMode = localStorage.getItem('lightMode');
+const enablelightMode = () => {
+
+  document.body.classList.add('lightmode');
+
+  localStorage.setItem("lightMode", "enabled");
+
+};
+
+const disablelightMode = () => {
+
+  document.body.classList.remove('lightmode');
+
+  localStorage.setItem("lightMode", null);
+
+};
+
+if (lightMode == 'enabled') {
+  enablelightMode();
+}
+
+function theme() {
+  lightMode = localStorage.getItem('lightMode');
+  if (lightMode !== "enabled") {
+    enablelightMode();
+  } else {
+    disablelightMode();
+  }
+}
