2d0c8f00bdc274411506598f99bf7ab8fab6aa68
devnesi <davi_nesi@hotmail.com>
2020-06-20 17:15:16 -0300

    add site array

--- loginMethods.js
+++ loginMethods.js
@@ -118,3 +118,51 @@ function register() {
         getLogin.style.display = "none";
     }
 }
+
+
+//------------- MONITORAMENTO --------------//
+
+
+const monitorForm = document.querySelector("#monitorForm");
+var sites = [];
+
+
+
+//Login
+monitorForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    site = getInputVal("endereco");
+    if(!isURL(site)){
+        return alert("Informe uma url valida")
+    }
+    addSite(site)
+});
+
+
+function addSite(site){
+    if(sites.indexOf(site)){
+        sites.push(site)
+
+        fetch("https://cors-anywhere.herokuapp.com/"+site).then(function(response) {
+            if(response.status==200){
+                console.log("Site Online")
+            } else {
+                //console.clear()
+                console.log("Site offline")
+            }
+          })
+    }
+}
+
+function isURL(str)
+{
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+}
