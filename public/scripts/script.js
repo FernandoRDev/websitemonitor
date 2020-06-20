b29fb9f43bd5c559f39df1e01f54516e3d67d286
fernandordev <fernandorodriguesdev@outlook.com>
2020-06-20 20:38:50 -0300

    fixes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,6 +21,7 @@ const registerForm = document.querySelector("#registerForm");
 
 const getRegister = document.getElementById("register");
 const getLogin = document.getElementById("login");
+const getDetail = document.getElementById("detail");
 
 btnUser = document.getElementById("btn-user")
 
@@ -30,28 +31,29 @@ function logout() {
     location.reload();
 }
 
-function recoveryPass(){
+function recoveryPass() {
     let loginEmail = getInputVal("login-email");
-    
-    if(loginEmail == '' || loginEmail == null){
+
+    if (loginEmail == '' || loginEmail == null) {
         return alert("Preencha o campo de e-mail para recuperar")
     }
-    
-    auth.sendPasswordResetEmail(loginEmail).then(function() {
+
+    auth.sendPasswordResetEmail(loginEmail).then(function () {
         return alert("E-mail de recuperação enviado")
-      }).catch(function(e) {
+    }).catch(function (e) {
         return alert(e.message)
-      });
+    });
 }
 
-
 //Listing change of user
-auth.onAuthStateChanged(function (user){   
-    let bntLogout = document.getElementById("btn-logout")
+auth.onAuthStateChanged(function (user) {
+    let btnLogout = document.getElementById("btn-logout")
+    let btnLogin = document.getElementById("btn-user")
 
-    if (user){
+    if (user) {
         btnUser.textContent = (user.email).split("@")[0]
-        bntLogout.style.display = "block";
+        btnLogout.style.display = "block";
+        btnLogin.removeAttribute('onclick');
     }
 });
 
@@ -60,15 +62,15 @@ loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const loginPass = getInputVal("login-password");
     const loginEmail = getInputVal("login-email");
-  
+
     firebase.auth().signInWithEmailAndPassword(loginEmail, loginPass).then((cred) => {
-        loginForm.reset();
-        getLogin.style.display = "none";
-      })
-      .catch((e) => {
-        return alert(e.message)//TODO: feedback login errado kek
-      });
-  });
+            loginForm.reset();
+            getLogin.style.display = "none";
+        })
+        .catch((e) => {
+            return alert(e.message) //TODO: feedback login errado kek
+        });
+});
 
 //Registro
 registerForm.addEventListener("submit", (e) => {
@@ -76,19 +78,19 @@ registerForm.addEventListener("submit", (e) => {
     const registerPass = getInputVal("register-password");
     const repeatPass = getInputVal("repeat-password");
     const registerEmail = getInputVal("register-email");
-  
-    if(registerPass != repeatPass){
+
+    if (registerPass != repeatPass) {
         return alert("errou brother");
     }
 
     firebase.auth().createUserWithEmailAndPassword(registerEmail, registerPass).then((cred) => {
-        loginForm.reset();
-        getRegister.style.display = "none";
-      })
-      .catch((e) => {
-        return alert(e.message)
-      });
-  });
+            loginForm.reset();
+            getRegister.style.display = "none";
+        })
+        .catch((e) => {
+            return alert(e.message)
+        });
+});
 
 // Function to get form values
 function getInputVal(id) {
@@ -102,9 +104,9 @@ function login() {
     } else {
         if (getRegister.style.display === "flex") {
             getRegister.style.display = "none";
-        }else{
+        } else {
             getLogin.style.display = "flex";
-            getRegister.style.display = "none";            
+            getRegister.style.display = "none";
         }
     }
 }
@@ -130,15 +132,14 @@ var sites = [];
 monitorForm.addEventListener("submit", (e) => {
     e.preventDefault()
     site = getInputVal("endereco");
-    if(!isURL(site)){
+    if (!isURL(site)) {
         return alert("Informe uma url valida")
     }
-    site = site.replace('https://','')
-    site = site.replace('http://','')
-    site = site.replace('www.','')
-    site = site.replace('ww2.','')
-    if(site.includes('/'))
-    {
+    site = site.replace('https://', '')
+    site = site.replace('http://', '')
+    site = site.replace('www.', '')
+    site = site.replace('ww2.', '')
+    if (site.includes('/')) {
         site = site.split('/')[0]
     }
     console.log(site)
@@ -147,8 +148,8 @@ monitorForm.addEventListener("submit", (e) => {
 });
 
 
-function addSite(site){
-    if(sites.indexOf(site)){
+function addSite(site) {
+    if (sites.indexOf(site)) {
         sites.push(site)
     } else {
         return
@@ -160,58 +161,54 @@ function addSite(site){
     var cell2 = row.insertCell(1);
 
 
-    fetch("https://cors-anywhere.herokuapp.com/"+site).then(function(response) {
-        if(response.status==200){
-            cell1.innerHTML = '<div> <img src="http://www.google.com/s2/favicons?domain='+site+'"/>'+site+'</div>'
+    fetch("https://cors-anywhere.herokuapp.com/" + site).then(function (response) {
+        if (response.status == 200) {
+            cell1.innerHTML = '<div> <img src="http://www.google.com/s2/favicons?domain=' + site + '"/>' + site + '</div>'
             cell2.innerHTML = '<div> <img src="assets/on.png" alt="icon"/> online</div>'
-        
+
         } else {
-            cell1.innerHTML = '<div> <img src="http://www.google.com/s2/favicons?domain='+site+'"/>'+site+'</div>'
+            cell1.innerHTML = '<div> <img src="http://www.google.com/s2/favicons?domain=' + site + '"/>' + site + '</div>'
             cell2.innerHTML = '<div> <img src="assets/off.png" alt="icon"/>offline</div>'
         }
-      }).catch(function(e) {
-        cell1.innerHTML = '<div> <img src="http://www.google.com/s2/favicons?domain='+site+'"/>'+site+'</div>'
+    }).catch(function (e) {
+        cell1.innerHTML = '<div> <img src="http://www.google.com/s2/favicons?domain=' + site + '"/>' + site + '</div>'
         cell2.innerHTML = '<div> <img src="assets/off.png" alt="icon"/>offline</div>'
-      });
-         
+    });
+
 }
 
 
 
-function isURL(str)
-{
-  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-        if (regexp.test(str))
-        {
-          return true;
-        }
-        else
-        {
-          return false;
-        }
+function isURL(str) {
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function sleep (time) {
+function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-  function loop(){
+function loop() {
     sleep(5000).then(() => {
         console.log("eho")
         sites.forEach(site => {
-            fetch("https://cors-anywhere.herokuapp.com/"+site).then(function(response) {
-                if(response.status==200){
+            fetch("https://cors-anywhere.herokuapp.com/" + site).then(function (response) {
+                if (response.status == 200) {
                     console.log("Site Online")
                 } else {
                     //console.clear()
                     console.log("Site offline")
                 }
-              })
+            })
 
         });
         loop();
-     });
+    });
 
 }
 
-  //loop();
+//loop();
